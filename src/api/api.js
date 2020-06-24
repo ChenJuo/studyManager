2967a4b4958ffb8422f82ab21ce8c8a851a2cd3b
陈卓 <jingxiaofeng1023@gmail.com>
2020-06-24 20:05:57 +0800

    修改登录

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 //axios用于提供get数据的一种连接的接口
 import axios from 'axios';
+import de from "element-ui/lib/locale/lang/de";
 
-let base = '';
+let base = 'localhost:8443/ib';
 
 //发送用户登录信息验证的请求的方法
 export const requestLogin = params => {
